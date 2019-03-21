189210826cf04c0ba81f45bcf30a54f92c6faeb9
Georg Federmann <georg.federmann@gmail.com>
2019-03-21 22:25:29 +0100

    Playing with EOF and getchar()

---
+++ CompareEof.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// show that getchar() == EOF is either 0 or 1.
+// Program can be run like so:
+// cat data/words | bin/CompareEof
+int main(void){
+  int c;
+  do {
+    c = (getchar() == EOF);
+    printf("Value of (getchar() == EOF) is %d.\n", c);
+  } while ( c != 1 );
+}
---
+++ PrintEofValue.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+// prints the value of stdio constant EOF.
+int main(void){
+  printf("EOF has the value %d.\n", EOF);
+  return 0;
+}
