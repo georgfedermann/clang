6eead47ee8476e925b654f45a8a0d0e360c73e66
Georg Federmann <georg.federmann@rbinternational.com>
2019-03-27 19:09:55 +0100

    Slightly improve wc tool
    by introducing state machine for word boundaries
    to return same results as gnu wc for ASCII text files.

--- wc.c
+++ wc.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <string.h>
 // count characters in input
+// use states to define whether current position is within or out
+// of the boundaries of a word. If out of boundaries the next non-
+// whitespace character would mark the beginning of the next word.
+#define WITHIN_WORD_BOUNDARIES 0
+#define OUTSIDE_WORD_BOUNDARIES 1
 int main(void) {
   long characterCount = 0;
   long lineCount = 0;
   long wordCount = 0;
   int character;
+  int parserState = OUTSIDE_WORD_BOUNDARIES;
 
   while ((character = getchar()) != EOF) {
     characterCount++;
     if (character == ' ') {
-      wordCount++;
+      parserState = OUTSIDE_WORD_BOUNDARIES;
     } else if (((char)character) == '\n') {
-      wordCount++;
       lineCount++;
+      parserState = OUTSIDE_WORD_BOUNDARIES;
+    } else if (parserState == OUTSIDE_WORD_BOUNDARIES){
+      wordCount++;
+      parserState = WITHIN_WORD_BOUNDARIES;
     }
   }
 
