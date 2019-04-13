878ccd73db45c4ff2276c21eb5599d1ce71f7fc7
Georg Federmann <georg.federmann@gmail.com>
2019-04-13 14:13:04 +0200

    add tool to replace tabs with blanks

--- detab.c
+++ detab.c
@@ -4,19 +4,82 @@
 
 // max line length in characters
 #define MAX_LINE_LENGTH 120
+#define BLANKS_IN_TAB 7
 
-int readline(char currentLine[], int lineMaxLength);
+int readline(char currentLine[], int maxLineLength);
 
-void replaceTabs(char currentLine[], int lineMaxLength);
+void replaceTabs(char currentLine[]);
 
-int main(void){
-  
+// restores from to backup until '\0' is found or length is met
+void backup(char from[], char backup[], int start, int length);
+
+// restores from to target until '\0' is found
+void restore(char from[], char target[], int targetStartIndex);
+
+int main(void) {
   char currentLine[120];
   int maxLineLength = MAX_LINE_LENGTH, currentLineLength;
 
-  while ((currentLineLength = readline(currentLine, maxLineLength)) > 0){
-
+  while ((currentLineLength = readline(currentLine, maxLineLength)) > 0) {
+    replaceTabs(currentLine);
+    printf("%s", currentLine);
   }
 
   return 0;
+}
+
+int readline(char currentLine[], int maxLineLength) {
+  int counter, character;
+  for (counter = 0; counter < maxLineLength - 1 &&
+                    ((character = getchar())) != EOF && character != '\n';
+       counter++) {
+    currentLine[counter] = character;
+  }
+  if (character == '\n') {
+    currentLine[counter] = '\n';
+    counter++;
+  }
+  currentLine[counter] = '\0';
+  return counter;
+}
+
+// scans a string and replaces instances of tabs by a fitting amount of blanks.
+// the end of the string is marked by \0.
+void replaceTabs(char currentLine[]) {
+  // i .. counter, c .. character
+  int i, c, blankCount;
+  char backupArray[MAX_LINE_LENGTH];
+
+  for (i = 0; (c = currentLine[i]) != '\0'; i++) {
+    currentLine[i] = c == '\t' ? ' ' : c;
+    if (c == '\t') {
+      backup(currentLine, backupArray, i + 1, MAX_LINE_LENGTH - 1 - i);
+      replaceTabs(backupArray);
+      for (blankCount = 0; blankCount < BLANKS_IN_TAB; blankCount++) {
+        currentLine[i++] = ' ';
+      }
+      restore(backupArray, currentLine, i);
+    } else {
+      // do nothing
+    }
+  }
+}
+
+void backup(char from[], char backup[], int start, int length) {
+  // i .. counter, c .. character
+  int i, c;
+  for (i = start; i <= length && (c = from[i]) != '\0'; i++) {
+    backup[i - start] = c;
+  }
+  backup[i - start] = '\0';
+}
+
+// restores from to target until '\0' is found
+void restore(char from[], char target[], int targetStartIndex) {
+  // i .. counter, c .. character
+  int i;
+  for (i = targetStartIndex; from[i - targetStartIndex] != '\0'; i++) {
+    target[i] = from[i - targetStartIndex];
+  }
+  target[i] = '\0';
 }
